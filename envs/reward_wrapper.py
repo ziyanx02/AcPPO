a77ca93f090bad8fcd66cab3f1dc14e3de719fc9
ziyanx02 <ziyanx02@gmail.com>
2025-01-31 12:45:45 -0500

    add jump

--- envs/reward_wrapper.py
+++ envs/reward_wrapper.py
@@ -103,6 +103,80 @@ class Walk(LocoEnv):
         # Terminal reward / penalty
         return self.terminate_buf.float()
 
+class Jump(Walk):
+
+    def _prepare_obs_noise(self):
+        self.obs_noise[:3] = self.obs_cfg['obs_noise']['ang_vel']
+        self.obs_noise[3:6] = self.obs_cfg['obs_noise']['gravity']
+        self.obs_noise[6:18] = self.obs_cfg['obs_noise']['dof_pos']
+        self.obs_noise[18:30] = self.obs_cfg['obs_noise']['dof_vel']
+
+    def compute_observation(self):
+        phase = self.episode_length_buf.float().unsqueeze(1) / self.max_episode_length * 2 * np.pi
+        obs_buf = torch.cat(
+            [
+                self.base_ang_vel * self.obs_scales['ang_vel'],                     # 3
+                self.projected_gravity,                                             # 3
+                (self.dof_pos - self.default_dof_pos) * self.obs_scales['dof_pos'], # 12
+                self.dof_vel * self.obs_scales['dof_vel'],                          # 12
+                self.actions,                                                       # 12
+                torch.sin(phase),
+                torch.cos(phase),
+                torch.sin(phase / 2),
+                torch.cos(phase / 2),
+                torch.sin(phase / 4),
+                torch.cos(phase / 4),
+            ],
+            axis=-1,
+        )
+        # add noise
+        if not self.eval:
+            obs_buf += gs_rand_float(
+                -1.0, 1.0, (self.num_obs,), self.device
+            )  * self.obs_noise
+
+        clip_obs = 100.0
+        self.obs_buf = torch.clip(obs_buf, -clip_obs, clip_obs)
+
+    def compute_critic_observation(self):
+        phase = self.episode_length_buf.float().unsqueeze(1) / self.max_episode_length * 2 * np.pi
+        privileged_obs_buf = torch.cat(
+            [
+                self.base_pos[:, 2:3],                                              # 1
+                self.base_lin_vel * self.obs_scales['lin_vel'],                     # 3
+                self.base_ang_vel * self.obs_scales['ang_vel'],                     # 3
+                self.projected_gravity,                                             # 3
+                (self.dof_pos - self.default_dof_pos) * self.obs_scales['dof_pos'], # 12
+                self.dof_vel * self.obs_scales['dof_vel'],                          # 12
+                self.actions,                                                       # 12
+                self.last_actions,                                                  # 12
+                torch.sin(phase),
+                torch.cos(phase),
+                torch.sin(phase / 2),
+                torch.cos(phase / 2),
+                torch.sin(phase / 4),
+                torch.cos(phase / 4),
+            ],
+            axis=-1,
+        )
+        clip_obs = 100.0
+        self.privileged_obs_buf = torch.clip(privileged_obs_buf, -clip_obs, clip_obs)
+
+    def check_termination(self):
+        self.terminate_buf = (
+            self.episode_length_buf > self.max_episode_length
+        )
+        self.reset_buf = (
+            self.episode_length_buf > self.max_episode_length
+        )
+
+    def _prepare_temporal_distribution(self):
+        super()._prepare_temporal_distribution()
+        for i in range(50, 75):
+            self.state_mean[i, 4] = 2
+            self.state_mean[i, 6] = 9.8 * (62.5 - i) / 50
+            self.state_mean[i, 0] = 0.2 + 9.8 / 2 * (12.5 ** 2 - (62.5 - i) ** 2) / 50 ** 2
+
 class Backflip(Walk):
 
     def _prepare_obs_noise(self):
@@ -178,77 +252,4 @@ class Backflip(Walk):
             self.state_mean[i, 3] = -np.cos(flip_angle)
             self.state_mean[i, 8] = -4 * np.pi
             self.state_mean[i, 6] = 9.8 * (62.5 - i) / 50
-            self.state_mean[i, 0] += 9.8 / 2 * (12.5 ** 2 - (62.5 - i) ** 2) / 50 ** 2
-
-    def _reward_orientation_control(self):
-        # Penalize non flat base orientation
-        current_time = self.episode_length_buf * self.dt
-        phase = (current_time - 0.5).clamp(min=0, max=0.5)
-        quat_pitch = gs_quat_from_angle_axis(4 * phase * torch.pi,
-                                             torch.tensor([0, -1, 0], device=self.device, dtype=torch.float))
-
-        desired_base_quat = gs_quat_mul(quat_pitch, self.base_init_quat.reshape(1, -1).repeat(self.num_envs, 1))
-        inv_desired_base_quat = gs_inv_quat(desired_base_quat)
-        desired_projected_gravity = gs_transform_by_quat(self.global_gravity, inv_desired_base_quat)
-
-        orientation_diff = torch.sum(torch.square(self.projected_gravity - desired_projected_gravity), dim=1)
-
-        return orientation_diff
-
-    def _reward_ang_vel_y(self):
-        current_time = self.episode_length_buf * self.dt
-        ang_vel = -self.base_ang_vel[:, 1].clamp(max=7.2, min=-7.2)
-        return ang_vel * torch.logical_and(current_time > 0.5, current_time < 1.0)
-
-    def _reward_ang_vel_z(self):
-        return torch.abs(self.base_ang_vel[:, 2])
-
-    def _reward_lin_vel_z(self):
-        current_time = self.episode_length_buf * self.dt
-        lin_vel = self.robot.get_vel()[:, 2].clamp(max=3)
-        return lin_vel * torch.logical_and(current_time > 0.5, current_time < 0.75)
-
-    def _reward_height_control(self):
-        # Penalize non flat base orientation
-        current_time = self.episode_length_buf * self.dt
-        target_height = 0.3
-        height_diff = torch.square(target_height - self.base_pos[:, 2]) * torch.logical_or(current_time < 0.4, current_time > 1.4)
-        return height_diff
-
-    def _reward_actions_symmetry(self):
-        actions_diff = torch.square(self.actions[:, 0] + self.actions[:, 3])
-        actions_diff += torch.square(self.actions[:, 1:3] - self.actions[:, 4:6]).sum(dim=-1)
-        actions_diff += torch.square(self.actions[:, 6] + self.actions[:, 9])
-        actions_diff += torch.square(self.actions[:, 7:9] - self.actions[:, 10:12]).sum(dim=-1)
-        return actions_diff
-    
-    def _reward_gravity_y(self):
-        return torch.square(self.projected_gravity[:, 1])
-
-    def _reward_feet_distance(self):
-        current_time = self.episode_length_buf * self.dt
-        cur_footsteps_translated = self.foot_positions - self.base_pos.unsqueeze(1)
-        footsteps_in_body_frame = torch.zeros(self.num_envs, 4, 3, device=self.device)
-        for i in range(4):
-            footsteps_in_body_frame[:, i, :] = gs_quat_apply(gs_quat_conjugate(self.base_quat),
-                                                                 cur_footsteps_translated[:, i, :])
-
-        stance_width = 0.3 * torch.ones([self.num_envs, 1,], device=self.device)
-        desired_ys = torch.cat([stance_width / 2, -stance_width / 2, stance_width / 2, -stance_width / 2], dim=1)
-        stance_diff = torch.square(desired_ys - footsteps_in_body_frame[:, :, 1]).sum(dim=1)
-        
-        return stance_diff
-
-    def _reward_feet_height_before_backflip(self):
-        current_time = self.episode_length_buf * self.dt
-        foot_height = (self.foot_positions[:, :, 2]).view(self.num_envs, -1) - 0.02
-        return foot_height.clamp(min=0).sum(dim=1) * (current_time < 0.5)
-
-    def _reward_collision(self):
-        # Penalize collisions on selected bodies
-        current_time = self.episode_length_buf * self.dt
-        return (1.0 * (torch.norm(self.link_contact_forces[:, self.penalized_contact_link_indices, :], dim=-1) > 0.1)).sum(dim=1)
-
-    def _reward_dof_pos_diff(self):
-        # Penalize dof positions deviate from default pose
-        return torch.square(self.dof_pos - self.default_dof_pos).sum(dim=1)
+            self.state_mean[i, 0] = 0.2 + 9.8 / 2 * (12.5 ** 2 - (62.5 - i) ** 2) / 50 ** 2
